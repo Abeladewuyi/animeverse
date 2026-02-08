e28c8ad3a1addd04eefb24a37cb61aedb2cb7acd
Abel <abeladewuyi8@gmail.com>
2026-02-08 07:16:01 -0800

    Fixed Firebase signup import

--- animeverse/src/components/Signup.jsx
+++ animeverse/src/components/Signup.jsx
@@ -3,7 +3,7 @@ import animeBg from "../assets/anime-bg.png";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
