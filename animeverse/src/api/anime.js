93d8c334fb320c1355abfe045f3b49ecc342e9b8
Abel <abeladewuyi8@gmail.com>
2026-02-23 15:52:51 -0800

    fix top anime to show currently airing

--- animeverse/src/api/anime.js
+++ animeverse/src/api/anime.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://api.jikan.moe/v4";
 
 export const getTopAnime = async () => {
-  const res = await fetch(`${BASE_URL}/top/anime?limit=20`);
+  const res = await fetch(`${BASE_URL}/top/anime?filter=airing&limit=20`);
   const data = await res.json();
   return data.data;
 };
