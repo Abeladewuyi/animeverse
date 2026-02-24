7233b4932695c7462d770866850762a53e263646
Abel <abeladewuyi8@gmail.com>
2026-02-23 17:18:40 -0800

    404 error fix

--- animeverse/src/main.jsx
+++ animeverse/src/main.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ActivityProvider } from "./context/ActivityContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <ActivityProvider>
           <App />
         </ActivityProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
